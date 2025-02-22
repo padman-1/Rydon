c80bf72983c425aa7b09113d2536185159584578
Padman <frimpongy890@gmail.com>
2025-02-22 16:34:41 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.rydon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:rydon/app/app.dart';
+import 'package:rydon/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
