4644ef2d469224e62c012e110a2e0b93132c6c4a
Padman <frimpongy890@gmail.com>
2025-02-24 17:46:31 +0000

    created colors util

---
+++ lib/util/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF00C6FF);
+  static const Color secondary = Color(0xFF00C6FF);
+  static const Color background = Color(0xFF00C6FF);
+  static const Color surface = Color(0xFF00C6FF);
+  static const Color error = Color(0xFF00C6FF);
+  static const Color onPrimary = Color(0xFF00C6FF);
+  static const Color onSecondary = Color(0xFF00C6FF);
+  static const Color onBackground = Color(0xFF00C6FF);
+  static const Color onSurface = Color(0xFF00C6FF);
+  static const Color onError = Color(0xFF00C6FF);
+  static const Color scaffoldBackground = Color(0xFF00C6FF);
+  static const Color divider = Color(0xFF00C6FF);
+  static const Color mainButton = Color(0XFF282420);
+  static const Color signUpWith = Color(0XFF8E8E93);
+  static const Color textField = Color(0XFFE4E4DD);
+  static const Color continueButton = Color(0XFF969179);
+  static const Color textFieldHint = Color(0XFF9F9696);
+}
