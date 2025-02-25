95ed3a7df705e53f4d7584b0f63336a74ace4e88
Padman <frimpongy890@gmail.com>
2025-02-25 15:04:08 +0000

    added theme data to main development APP

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rydon/counter/counter.dart';
 import 'package:rydon/l10n/l10n.dart';
-import 'package:rydon/pages/widgets/custom_textfield.dart';
+import 'package:rydon/pages/sign%20up/view/sign_up_page.dart';
+import 'package:rydon/widgets/custom_textfield.dart';
+import 'package:rydon/util/colors.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -13,14 +16,31 @@ class App extends StatelessWidget {
         appBarTheme: AppBarTheme(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
+        textTheme: GoogleFonts.poppinsTextTheme().copyWith(
+            // displayLarge: GoogleFonts.poppins(
+            //   fontSize: 12,
+            //   color: AppColors.signUpWith,
+            // ),
+            ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.divider,
+            foregroundColor: Colors.white,
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
         useMaterial3: true,
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CustomTextField(
-        hintext: 'Ex: John',
-        isPassword: true,
-      ),
+      home: const SignUpPage(),
+      // home: const CustomTextField(
+      //   hintext: 'Ex: John',
+      //   isPassword: true,
+      // ),
     );
   }
 }
