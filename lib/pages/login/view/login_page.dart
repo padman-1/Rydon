9bc6f13c263ba3c84b46a6599286ea4dd9fb7b97
Padman <frimpongy890@gmail.com>
2025-02-26 22:18:01 +0000

    created login page

---
+++ lib/pages/login/view/login_page.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:rydon/util/colors.dart';
+import 'package:rydon/widgets/custom_textfield.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final devSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Sign In',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Hi Welcome back, you've been missed",
+                  textAlign: TextAlign.center,
+                  // style: context.getTextTheme.bodyLarge,
+                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        color: AppColors.signUpWith,
+                      ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Email',
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      CustomTextField(
+                        hintext: 'example@gmail.com',
+                      ),
+                      Text(
+                        'Password',
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      CustomTextField(
+                        hintext: '123456789',
+                        isPassword: true,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          'Forgot Password?',
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: AppColors.signUpWith,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: devSize.width / 2,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  'Sign In',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 25,
+                            ),
+                            SizedBox(
+                              width: devSize.width / 2,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Expanded(
+                                    child: Divider(
+                                      color: AppColors.divider, // Line color
+                                      thickness: 1, // Line thickness
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 5,
+                                    ),
+                                    child: Text(
+                                      'or sign up with',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall
+                                          ?.copyWith(
+                                            color: AppColors.signUpWith,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                    ),
+                                  ),
+                                  const Expanded(
+                                    child: Divider(
+                                      color: AppColors.divider, // Line color
+                                      thickness: 1, // Line thickness
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            SizedBox(
+                              width: devSize.width / 2,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: AppColors.socialsBorder,
+                                      ),
+                                      borderRadius: BorderRadius.circular(17),
+                                    ),
+                                    child:
+                                        Image.asset('assets/images/apple.png'),
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: AppColors.socialsBorder,
+                                      ),
+                                      borderRadius: BorderRadius.circular(17),
+                                    ),
+                                    child:
+                                        Image.asset('assets/images/google.png'),
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: AppColors.socialsBorder,
+                                      ),
+                                      borderRadius: BorderRadius.circular(17),
+                                    ),
+                                    child: Image.asset(
+                                        'assets/images/facebook.png'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: "Don't have an account? ",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall),
+                                  TextSpan(
+                                    text: 'Sign Up',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodySmall!
+                                        .copyWith(
+                                          color: AppColors.signUpWith,
+                                          decoration: TextDecoration.underline,
+                                        ),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                const LoginPage(),
+                                          ),
+                                        );
+                                      },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
